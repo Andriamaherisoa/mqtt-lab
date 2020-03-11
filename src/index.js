96cc1ffe70e917f66dd43abaa1325043bd34ac51
HOPE <hopewebcup@gmail.com>
2020-03-11 05:47:02 +0100

    feat: Connect to docker image
    - Changed socket connection url
    - Add sensor models and types and test from last TP
    - Create function utils

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
-import './index.css';
+import "./index.css";
+import { createSensor } from "./models/sensors";
+import { sensorAlreadyExist, findSensorIndex } from "./utils/sensorUtils";
 
-const url  = "ws://" + window.location.host+ "/socket";
+const mqtt = require("mqtt");
+const url = "ws://" + window.location.host + "/socket";
+let client = mqtt.connect(url);
+const sensors = [];
+const buffer = [];
 
+client.on("connect", function() {
+  client.subscribe("#", err => {
+    if (!err) {
+      console.log("client connected to Mqtt server via ", url);
+    }
+  });
+});
 
-// On pourrait utiliser un WebSocket pour se connecter au serveur MQTT
-// Mais on préférera utiliser un bibliothèque spécifique au protocole. 
-// 
-// Ne pas utiliser directement une WebSocket...
-const ws = new WebSocket(url);
-ws.onclose = (event) => console.log("Direct connection closed. Cool!")
-ws.onopen = (event) => {
-  console.log("We are connected. But let's NOT use this connection because we want to use a MQTT library to handle the WebSocket.");
-  ws.close()
-};
+client.on("close", () => {
+  console.log("mqtt connection closed");
+});
 
+client.on("error", () => {
+  console.warn("Error found");
+});
+
+client.on("message", (topic, message) => {
+  let id = parseInt(topic.split("/")[1]);
+
+  let data = JSON.parse(message);
+  let sensor = createSensor({ ...data, id });
+  if (sensorAlreadyExist(sensors, sensor)) {
+    let sensorIndex = findSensorIndex(sensors, sensor.name);
+    let value = data.value;
+    sensors[sensorIndex].addTimeSerie({
+      value,
+      label: new Date().toISOString()
+    });
+  } else {
+    sensors.push(sensor);
+  }
+});
